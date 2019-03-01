a642f552198c31bceb22bbb9c0725dbb14de4f13
zxkeep <18281604336@163.com>
2019-03-01 12:50:04 +0800

    回顾hashtable 底层原理

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
+import java.util.Hashtable;
+
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("hello world!!!");
 		int i = 0;
-	}
+		Hashtable<Object, Object> myHsahTable = new Hashtable<>();
+    }
 }
